0c146b39040c9b6095d0ffee0d6a5c77ea671d97
Terry <tchan247@gmail.com>
2015-08-17 23:03:47 -0700

    Implement rotateMatrix

--- practiceProblems.js
+++ practiceProblems.js
@@ -22,7 +22,7 @@ var toFraction = function(number) {
   gcd = right;
   
   // find greatest common denominator
-  while (tens % gcd !== 0 || right % gcd !== 0 && gcd > 0) {
+  while (tens % gcd !== 0 || right % gcd !== 0 && gcd > 1) {
     gcd--;
   }
   
@@ -33,4 +33,67 @@ var toFraction = function(number) {
 
   return num + '/' + den;
 };
+
+// Time: O(n)
+// Space: O(1)
+
+
+/** 
+ * Rotate an NxN matrix 90 degrees
+ * 
+ * Example:
+ *
+ * var matrix = 
+ *  [[1,2],
+ *   [3,4]];
+ *
+ * rotateMatrix(matrix) ->
+ *  [[3,1]]  
+ *   [4,2]];
+ * 
+ * Extra: 
+ * - Make function able to rotate MxN rectangles
+ * - Make function accept extra parameter: 1 for clockwise & -1 for counterclockwise
+*/
+
+var rotateMatrix = function(matrix, direction) {
+ var newMatrix = [];
+ var len1 = matrix.length;
+ var i, j, dest, x, y, len2;
+ 
+ // handle size 0 matrix
+ if(len1 === 0) {
+   return [];
+ }
+ 
+ len2 = matrix[0].length;
+ 
+ // check for valid direction. default to 1 (clockwise)
+ direction = direction === 1 || direction === -1? direction : 1;
+ 
+ for(i = 0; i < len1; i++) {
+   for(j = 0; j < len2; j++) {
+     // create an object with indicies depending on direction
+     dest = {
+       '1': {x: j, y: len1 - i - 1},
+       '-1': {x: len2 - j - 1, y: i}
+     };
+     
+     x = dest[direction].x;
+     y = dest[direction].y;
+     
+     // initialize array for index if not already there
+     if(!Array.isArray(newMatrix[x])) {
+       newMatrix[x] = [];
+     }
+     
+     newMatrix[x][y] = matrix[i][j];
+   }
+ }
+ 
+ return newMatrix;
+};
+
+// Time: O(n^2)
+// Space: O(n^2)
 
